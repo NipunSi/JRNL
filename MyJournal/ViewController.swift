64b4681d19416bb596bd63ed3d5dc081148ee68f
Nipun Singh <nipunbusiness@gmail.com>
2020-11-13 12:33:09 -0800

    Initial Commit

---
+++ MyJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyJournal
+//
+//  Created by Nipun Singh on 11/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
